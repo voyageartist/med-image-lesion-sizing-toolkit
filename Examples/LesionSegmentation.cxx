999c95d72bfb22537d2d80f3cba4e9656055144b
voyageartist <voyageartist@proton.me>
2022-07-16 16:45:15 +0000

    Updated the tiledb feedstock.

--- Examples/LesionSegmentation.cxx
+++ Examples/LesionSegmentation.cxx
@@ -25,4 +25,42 @@
 #include "vtkImagePlaneWidget.h"
 #include "vtkCellPicker.h"
 #include "vtkPolyDataNormals.h"
-#
+#include "vtkInteractorStyleTrackballCamera.h"
+#include "vtkOutlineSource.h"
+#include "vtkCommand.h"
+#include "vtkWindowToImageFilter.h"
+#include "vtkPNGWriter.h"
+#include "itkOrientImageFilter.h"
+#include "vtkVersion.h"
+
+// This needs to come after the other includes to prevent the global definitions
+// of PixelType to be shadowed by other declarations.
+#include "itkLesionSegmentationImageFilter8.h"
+#include "itkLesionSegmentationCommandLineProgressReporter.h"
+#include "LesionSegmentationCLI.h"
+
+#define VTK_CREATE(type, name) vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
+
+// --------------------------------------------------------------------------
+class SwitchVisibilityCallback : public vtkCommand
+{
+public:
+  static SwitchVisibilityCallback *
+  New()
+  {
+    return new SwitchVisibilityCallback;
+  }
+
+  void
+  SetActor(vtkActor * aActor)
+  {
+    this->Actor = aActor;
+  }
+  void
+  SetRenderWindow(vtkRenderWindow * aRenWin)
+  {
+    this->RenWin = aRenWin;
+  }
+
+  virtual void
+  Execu
