0dcef20150dab0f28f554036dfdadb16ca9e6c80
voyageartist <voyageartist@proton.me>
2022-08-18 21:05:12 +0000

    fixes #35

--- include/itkCannyEdgeDetectionRecursiveGaussianImageFilter.h
+++ include/itkCannyEdgeDetectionRecursiveGaussianImageFilter.h
@@ -3,4 +3,27 @@
  *  Copyright NumFOCUS
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in comp
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkCannyEdgeDetectionRecursiveGaussianImageFilter_h
+#define itkCannyEdgeDetectionRecursiveGaussianImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkImage.h"
+#include "itkFixedArray.h"
+#include "itkConstNeighborhoodIterator.h"
+#include "itkSmoothingRecursiveGaussianImageFilter.h"
+#include "itkMultiplyImageFilter.h"
+#include "itkZeroFluxNeumannBoundaryCondition.h"
+#include "itkMultiThreaderBase.h"
+#include "itkDerivativeOpe
