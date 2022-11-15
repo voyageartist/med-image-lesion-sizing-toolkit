cb46ca12982b4cb20f8d8b20a7b521bee87b8007
voyageartist <voyageartist@proton.me>
2022-11-15 06:52:54 +0000

    more changes, what do you want me to say?

--- include/itkCannyEdgesFeatureGenerator.hxx
+++ include/itkCannyEdgesFeatureGenerator.hxx
@@ -12,4 +12,37 @@
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
+ *=========================================================================*/
+#ifndef itkCannyEdgesFeatureGenerator_hxx
+#define itkCannyEdgesFeatureGenerator_hxx
+
+
+
+namespace itk
+{
+
+/**
+ * Constructor
+ */
+template <unsigned int NDimension>
+CannyEdgesFeatureGenerator<NDimension>::CannyEdgesFeatureGenerator()
+{
+  this->SetNumberOfRequiredInputs(1);
+
+  this->m_CastFilter = CastFilterType::New();
+  this->m_RescaleFilter = RescaleFilterType::New();
+  this->m_CannyFilter = CannyEdgeFilterType::New();
+
+  typename OutputImageSpatialObjectType::Pointer outputObject = OutputImageSpatialObjectType::New();
+
+  this->ProcessObject::SetNthOutput(0, outputObject.GetPointer());
+
+  this->m_Sigma.Fill(1.0);
+  this->m_UpperThreshold = NumericTraits<InternalPixelType>::max();
+  this->m_LowerThreshold = NumericTraits<InternalPixelType>::min();
+
+  this->m_RescaleFilter->SetOutputMinimum(1.0);
+  this->m_RescaleFilter->SetOutputMaximum(0.0);
+
