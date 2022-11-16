e7350dac18b9b207e9381b826bf99f922afa70ed
voyageartist <voyageartist@proton.me>
2022-11-16 21:25:01 +0000

    ext4: fix BUG_ON in mb_free_blocks()

--- include/itkConfidenceConnectedSegmentationModule.h
+++ include/itkConfidenceConnectedSegmentationModule.h
@@ -11,4 +11,33 @@
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the L
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkConfidenceConnectedSegmentationModule_h
+#define itkConfidenceConnectedSegmentationModule_h
+
+#include "itkRegionGrowingSegmentationModule.h"
+#include "itkConfidenceConnectedImageFilter.h"
+
+namespace itk
+{
+
+/** \class ConfidenceConnectedSegmentationModule
+ * \brief This class applies the connected threshold region growing
+ * segmentation method.
+ *
+ * SpatialObjects are used as inputs and outputs of this class.
+ *
+ * \ingroup SpatialObjectFilters
+ * \ingroup LesionSizingToolkit
+ */
+template <unsigned int NDimension>
+class ITK_TEMPLATE_EXPORT ConfidenceConnectedSegmentationModule : public RegionGrowingSegmentationModule<NDimension>
+{
+public:
+  ITK_DISALLOW_COPY_AND_MOVE(ConfidenceConnectedSegmentationModule);
+
+  /** Standard class type alias. */
+  using Self = ConfidenceConnectedSegmentatio
