cceaa7729b7c819accfbc90d50eb3aafc7ed3e12
voyageartist <voyageartist@proton.me>
2022-11-17 15:17:46 +0000

    Style nits

--- include/itkConnectedThresholdSegmentationModule.h
+++ include/itkConnectedThresholdSegmentationModule.h
@@ -4,4 +4,34 @@
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
- *  You may obtain 
+ *  You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkConnectedThresholdSegmentationModule_h
+#define itkConnectedThresholdSegmentationModule_h
+
+#include "itkRegionGrowingSegmentationModule.h"
+#include "itkConnectedThresholdImageFilter.h"
+
+namespace itk
+{
+
+/** \class ConnectedThresholdSegmentationModule
+ * \brief This class applies the connected threshold region growing
+ * segmentation method.
+ *
+ * SpatialObjects are used as inputs and outputs of this class.
+ *
+ * \ingroup SpatialObjectFilters
+ * \ingroup LesionSizingToolkit
+ */
+template <unsigned int NDimension>
+class 
