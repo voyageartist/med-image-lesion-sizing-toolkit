3f85354e742f2b456a7ab69c0ef1f1eacecac1f1
voyageartist <voyageartist@proton.me>
2022-11-25 17:05:49 +0000

    express

--- include/itkDescoteauxSheetnessImageFilter.h
+++ include/itkDescoteauxSheetnessImageFilter.h
@@ -15,4 +15,43 @@
  *  limitations under the License.
  *
  *=========================================================================*/
-#ifndef itkDescoteauxSheetnessIma
+#ifndef itkDescoteauxSheetnessImageFilter_h
+#define itkDescoteauxSheetnessImageFilter_h
+
+#include "itkUnaryFunctorImageFilter.h"
+#include "itkMath.h"
+
+namespace itk
+{
+
+/** \class DescoteauxSheetnessImageFilter
+ *
+ * \brief Computes a measure of Sheetness from the Hessian Eigenvalues
+ *
+ * Based on the "Sheetness" measure proposed by Decouteaux et. al.
+ *
+ * M.Descoteaux, M.Audette, K.Chinzei, el al.:
+ * "Bone enhancement filtering: Application to sinus bone segmentation
+ *  and simulation of pituitary surgery."
+ *  In: MICCAI.  (2005) 9-16
+ *
+ * \ingroup IntensityImageFilters  Multithreaded
+ * \ingroup LesionSizingToolkit
+ */
+namespace Function
+{
+
+template <typename TInput, typename TOutput>
+class Sheetness
+{
+public:
+  Sheetness()
+  {
+    m_Alpha = 0.5; // suggested value in the paper
+    m_Gamma = 0.5; // suggested value in the paper;
+    m_C = 1.0;
+    m_DetectBrightSheets = true;
+  }
+  ~Sheetness() = default;
+  bool
+  operator!=(const Sheetness &)
