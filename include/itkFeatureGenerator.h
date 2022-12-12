246c829845a3653ddd17490db36afd642d0b8595
voyageartist <voyageartist@proton.me>
2022-12-12 22:34:34 +0000

    Updated version.

--- include/itkFeatureGenerator.h
+++ include/itkFeatureGenerator.h
@@ -32,4 +32,35 @@ namespace itk
  * The typical use of this class would be to generate the edge-map needed by a
  * Level Set filter to internally compute its speed image.
  *
- * SpatialObjects are used as inputs and o
+ * SpatialObjects are used as inputs and outputs of this class.
+ *
+ * \ingroup SpatialObjectFilters
+ * \ingroup LesionSizingToolkit
+ */
+template <unsigned int NDimension>
+class ITK_TEMPLATE_EXPORT FeatureGenerator : public ProcessObject
+{
+public:
+  ITK_DISALLOW_COPY_AND_MOVE(FeatureGenerator);
+
+  /** Standard class type alias. */
+  using Self = FeatureGenerator;
+  using Superclass = ProcessObject;
+  using Pointer = SmartPointer<Self>;
+  using ConstPointer = SmartPointer<const Self>;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(FeatureGenerator, ProcessObject);
+
+  /** Dimension of the space */
+  static constexpr unsigned int Dimension = NDimension;
+
+  /** Type of spatialObject that will be passed as input and output of this
+   * segmentation method. */
+  using SpatialObjectType = SpatialObject<NDimension>;
+  using SpatialObjectPointer = typename SpatialObjectType::Pointer;
+
+  /** Input data that w
