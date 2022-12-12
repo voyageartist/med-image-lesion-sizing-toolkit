7e0f683c35bffc6d04818a8a81996c0fdc04f45b
voyageartist <voyageartist@proton.me>
2022-12-12 18:53:31 +0000

    Update test command

--- include/itkFeatureGenerator.h
+++ include/itkFeatureGenerator.h
@@ -8,4 +8,28 @@
  *
  *         https://www.apache.org/licenses/LICENSE-2.0.txt
  *
- *  Unless required by applicable law 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkFeatureGenerator_h
+#define itkFeatureGenerator_h
+
+#include "itkProcessObject.h"
+#include "itkImage.h"
+#include "itkDataObjectDecorator.h"
+#include "itkSpatialObject.h"
+
+namespace itk
+{
+
+/** \class FeatureGenerator
+ * \brief Class that generates features (typically images) used as input for a segmentation method.
+ *
+ * The typical use of this class would be to generate the edge-map needed by a
+ * Level Set filter to internally compute its speed image.
+ *
+ * SpatialObjects are used as inputs and o
