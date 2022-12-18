130351a8f0e8042f048f59ddfae7ac2ba31e2d05
voyageartist <voyageartist@proton.me>
2022-12-18 11:23:26 +0000

    random: add and use memzero_explicit() for clearing data

--- include/itkGeodesicActiveContourLevelSetSegmentationModule.hxx
+++ include/itkGeodesicActiveContourLevelSetSegmentationModule.hxx
@@ -9,4 +9,40 @@
  *         https://www.apache.org/licenses/LICENSE-2.0.txt
  *
  *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkGeodesicActiveContourLevelSetSegmentationModule_hxx
+#define itkGeodesicActiveContourLevelSetSegmentationModule_hxx
+
+#include "itkGeodesicActiveContourLevelSetImageFilter.h"
+#include "itkProgressAccumulator.h"
+
+
+namespace itk
+{
+
+
+/**
+ * Constructor
+ */
+template <unsigned int NDimension>
+GeodesicActiveContourLevelSetSegmentationModule<NDimension>::GeodesicActiveContourLevelSetSegmentationModule() =
+  default;
+
+
+/**
+ * Destructor
+ */
+template <unsigned int NDimension>
+GeodesicActiveContourLevelSetSegmentationModule<NDimension>::~GeodesicActiveContourLevelSetSegmentationModule() =
+  default;
+
+
+/**
+ * PrintSelf
+ */
+template <uns
