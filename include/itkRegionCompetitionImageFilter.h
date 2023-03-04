6b7d7a702792c8fd00db88b10d0f1b768853a726
voyageartist <voyageartist@proton.me>
2023-03-04 14:03:27 +0000

    Add a test demonstrating a problem with long submodule paths

--- include/itkRegionCompetitionImageFilter.h
+++ include/itkRegionCompetitionImageFilter.h
@@ -14,4 +14,33 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *=====
+ *=========================================================================*/
+#ifndef itkRegionCompetitionImageFilter_h
+#define itkRegionCompetitionImageFilter_h
+
+#include "itkImage.h"
+#include "itkImageToImageFilter.h"
+
+#include <vector>
+
+namespace itk
+{
+
+/** \class RegionCompetitionImageFilter
+ *
+ * \brief Perform front-propagation from different starting labeled regions.
+ *
+ * The filter expects two inputs: One gray-scale image and a labeled image.
+ * The labels will be used as initial regions from which the fronts will be
+ * propagated until they collide with other labeled regions. Each labeled front
+ * will compete for pixels against other labels.
+ *
+ * \ingroup RegionGrowingSegmentation
+ * \ingroup LesionSizingToolkit
+ */
+template <typename TInputImage, typename TOutputImage>
+class RegionCompetitionImageFilter : public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+  ITK_DISALLOW_COPY_AND_MOVE(RegionCompetitionImageFilter);
+
