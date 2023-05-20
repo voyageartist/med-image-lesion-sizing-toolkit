7808aa087fd012308c1b6d041005c6972f027162
voyageartist <voyageartist@proton.me>
2023-05-20 06:36:59 +0000

    Add rule

--- include/itkShapeDetectionLevelSetSegmentationModule.h
+++ include/itkShapeDetectionLevelSetSegmentationModule.h
@@ -7,3 +7,31 @@
  *  You may obtain a copy of the License at
  *
  *         https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkShapeDetectionLevelSetSegmentationModule_h
+#define itkShapeDetectionLevelSetSegmentationModule_h
+
+#include "itkSinglePhaseLevelSetSegmentationModule.h"
+#include "itkShapeDetectionLevelSetImageFilter.h"
+
+namespace itk
+{
+
+/** \class ShapeDetectionLevelSetSegmentationModule
+ * \brief This class applies the ShapeDetectionLevelSet segmentation
+ * method.
+ *
+ * SpatialObjects are used as inputs and outputs of this class.
+ *
+ * \ingroup SpatialObjectFilters
+ * \ingroup LesionSizingToolkit
+ */
+template <unsigned int NDimension>
+class ITK_TEMPLATE_EXPOR
