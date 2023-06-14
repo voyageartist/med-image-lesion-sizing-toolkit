deb87c609f8f5651c22d5e23c4267c26959b451e
voyageartist <voyageartist@proton.me>
2023-06-14 08:53:50 +0000

    Version updated to 3.6.4

--- include/itkSigmoidFeatureGenerator.hxx
+++ include/itkSigmoidFeatureGenerator.hxx
@@ -11,4 +11,46 @@
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkSigmoidFeatureGenerator_hxx
+#define itkSigmoidFeatureGenerator_hxx
+
+#include "itkProgressAccumulator.h"
+
+namespace itk
+{
+
+/**
+ * Constructor
+ */
+template <unsigned int NDimension>
+SigmoidFeatureGenerator<NDimension>::SigmoidFeatureGenerator()
+{
+  this->SetNumberOfRequiredInputs(1);
+  this->SetNumberOfRequiredOutputs(1);
+
+  this->m_SigmoidFilter = SigmoidFilterType::New();
+
+  this->m_SigmoidFilter->ReleaseDataFlagOn();
+
+  typename OutputImageSpatialObjectType::Pointer outputObject = OutputImageSpatialObjectType::New();
+
+  this->ProcessObject::SetNthOutput(0, outputObject.GetPointer());
+
+  this->m_Alpha = -1.0;
+  this->m_Beta = 128.0;
+}
+
+
+/*
+ * Destructor
+ */
+template <unsigned int NDimension>
+SigmoidFeatureGenerator<NDimension>::~SigmoidFeatureGenerator() = default;
+
+template <unsigned int NDimension>
+void
+SigmoidFeat
