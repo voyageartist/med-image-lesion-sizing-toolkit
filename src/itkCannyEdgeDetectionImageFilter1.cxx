a3a79c40bb88cb8174f48b310ea346fa662decc2
voyageartist <voyageartist@proton.me>
2023-07-14 07:29:42 +0000

    Updated Hebrew translation

---
+++ src/itkCannyEdgeDetectionImageFilter1.cxx
@@ -0,0 +1,22 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    CannyEdgeDetectionImageFilter1.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or https://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#  pragma warning(disable : 4786)
+#endif
+
+#ifdef __BORLANDC__
+# 
