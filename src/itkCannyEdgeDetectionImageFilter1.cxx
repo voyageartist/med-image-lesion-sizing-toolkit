803e7aa480069bf4f233c3e52797f810e5feb6a3
voyageartist <voyageartist@proton.me>
2023-07-27 11:07:34 +0000

    chore(package): update mocha to version 3.4.2

--- src/itkCannyEdgeDetectionImageFilter1.cxx
+++ src/itkCannyEdgeDetectionImageFilter1.cxx
@@ -19,4 +19,52 @@
 #endif
 
 #ifdef __BORLANDC__
-# 
+#  define ITK_LEAN_AND_MEAN
+#endif
+
+#include "itkIncludeRequiredIOFactories.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCastImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkCannyEdgeDetectionImageFilter.h"
+
+#include <string>
+
+
+int
+main(int argc, char * argv[])
+{
+
+  RegisterRequiredFactories();
+
+  if (argc < 3)
+  {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " inputImage outputImage [sigma upperThrehold lowerThreshold]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  const char * inputFilename = argv[1];
+  const char * outputFilename = argv[2];
+
+  float sigma = 2.0;
+  float lowerThreshold = 10.0;
+  float upperThreshold = 20.0;
+
+  if (argc > 3)
+  {
+    sigma = std::stod(argv[3]);
+  }
+
+  if (argc > 4)
+  {
+    upperThreshold = std::stod(argv[4]);
+  }
+
+  if (argc > 5)
+  {
+    lowerThreshold = std::stod(argv[5]);
+  }
+
+  using InputPixelTy
