aebe8653b2052321ae1966941915e45e291fc94d
voyageartist <voyageartist@proton.me>
2023-08-16 14:01:39 +0000

    updated usage

--- src/itkIncludeRequiredIOFactories.h
+++ src/itkIncludeRequiredIOFactories.h
@@ -9,4 +9,26 @@
  *         https://www.apache.org/licenses/LICENSE-2.0.txt
  *
  *  Unless required by applicable law or agreed to in writing, software
- *  distribute
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef itkIncludeRequiredIOFactories_h
+#define itkIncludeRequiredIOFactories_h
+#include "itkGDCMImageIOFactory.h"
+#include "itkMetaImageIOFactory.h"
+#include "itkPNGImageIOFactory.h"
+#include "itkObjectFactoryBase.h"
+
+void
+RegisterRequiredFactories()
+{
+  itk::ObjectFactoryBase::RegisterFactory(itk::MetaImageIOFactory::New());
+  itk::ObjectFactoryBase::RegisterFactory(itk::GDCMImageIOFactory::New());
+  itk::ObjectFactoryBase::RegisterFactory(itk::PNGImageIOFactory::New());
+}
+
+#endif
---
+++ src/itkLaplacianRecursiveGaussianImageFilter.cxx
@@ -0,0 +1,5 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (th
