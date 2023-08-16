b242a744f7ccc2e30634ed3ef01c63053bb2cfb4
voyageartist <voyageartist@proton.me>
2023-08-16 14:58:50 +0000

    Update TODO list.

--- src/itkLaplacianRecursiveGaussianImageFilter.cxx
+++ src/itkLaplacianRecursiveGaussianImageFilter.cxx
@@ -2,4 +2,39 @@
  *
  *  Copyright NumFOCUS
  *
- *  Licensed under the Apache License, Version 2.0 (th
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#if defined(_MSC_VER)
+#  pragma warning(disable : 4786)
+#endif
+
+#include "itkIncludeRequiredIOFactories.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkLaplacianRecursiveGaussianImageFilter.h"
+
+#include <string>
+
+
+int
+main(int argc, char * argv[])
+{
+
+  RegisterRequiredFactories();
+
+  if (argc < 4)
+  {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr 
