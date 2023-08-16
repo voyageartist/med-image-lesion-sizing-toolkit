f2a41b23b152b3590b1ee25c72cc087db5639ed1
voyageartist <voyageartist@proton.me>
2023-08-16 09:49:13 +0000

    Removed log message

--- src/itkImageReadWrite.cxx
+++ src/itkImageReadWrite.cxx
@@ -11,4 +11,43 @@
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language g
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#if defined(_MSC_VER)
+#  pragma warning(disable : 4786)
+#endif
+
+#ifdef __BORLANDC__
+#  define ITK_LEAN_AND_MEAN
+#endif
+
+#include "itkIncludeRequiredIOFactories.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImage.h"
+
+int
+main(int argc, char ** argv)
+{
+
+  RegisterRequiredFactories();
+
+  // Verify the number of parameters in the command line
+  if (argc < 3)
+  {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " inputImageFile  outputImageFile " << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  using PixelType = unsigned char;
+  constexpr unsigned int Dimension = 3;
+
+  using ImageType = itk::Image<PixelType, Dimension>;
+
+  using ReaderType = itk::ImageFileReader<ImageType>;
+  using WriterType = itk::ImageFileWriter<ImageType>;
+
+  ReaderType::Pointer reader = Reader
