67f9d8a8a74e2d4dead48bf3e0f88ebb95e52500
voyageartist <voyageartist@proton.me>
2023-10-09 09:52:00 +0000

    Remove gitignore

--- test/itkFastMarchingSegmentationModuleTest1.cxx
+++ test/itkFastMarchingSegmentationModuleTest1.cxx
@@ -28,4 +28,32 @@ itkFastMarchingSegmentationModuleTest1(int argc, char * argv[])
 {
   if (argc < 3)
   {
-    std::cerr << "Missing parameters." << std::en
+    std::cerr << "Missing parameters." << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " landmarksFile featureImage outputImage ";
+    std::cerr << " stoppingValue";
+    std::cerr << " distanceFromSeeds" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  constexpr unsigned int Dimension = 3;
+  using SegmentationModuleType = itk::FastMarchingSegmentationModule<Dimension>;
+
+  using FeatureImageType = SegmentationModuleType::FeatureImageType;
+  using OutputImageType = SegmentationModuleType::OutputImageType;
+
+  using FeatureReaderType = itk::ImageFileReader<FeatureImageType>;
+  using OutputWriterType = itk::ImageFileWriter<OutputImageType>;
+
+  using LandmarksReaderType = itk::LandmarksReader<Dimension>;
+
+  LandmarksReaderType::Pointer landmarksReader = LandmarksReaderType::New();
+
+  landmarksReader->SetFileName(argv[1]);
+  ITK_TRY_EXPECT_NO_EXCEPTION(landmarksReader->Update());
+
+
+  FeatureReaderType::Pointer featureReader = FeatureReaderType::New();
+  featureReader->SetFileName(argv[2]);
+
+  ITK_TRY_EX
