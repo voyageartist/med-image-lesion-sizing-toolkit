cbcea3a40bcada8a2507a3f8d2948f6ff1fe64fe
voyageartist <voyageartist@proton.me>
2023-10-12 17:47:04 +0000

    Upgraded to latest Rust-Empty

--- test/itkLandmarksReaderTest1.cxx
+++ test/itkLandmarksReaderTest1.cxx
@@ -5,4 +5,39 @@
 
   Copyright (c) Kitware Inc.
   All rights reserved.
-  See Copyright.txt or https://www.kitware.com/Copyright.htm f
+  See Copyright.txt or https://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "itkLandmarksReader.h"
+#include "itkSpatialObject.h"
+#include "itkSpatialObjectReader.h"
+#include "itkTestingMacros.h"
+
+
+int
+itkLandmarksReaderTest1(int argc, char * argv[])
+{
+  if (argc < 2)
+  {
+    std::cerr << "Missing parameters." << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " landmarksFile ";
+    return EXIT_FAILURE;
+  }
+
+
+  constexpr unsigned int Dimension = 3;
+
+  using InputSpatialObjectType = itk::LandmarkSpatialObject<Dimension>;
+
+  //
+  //  Reading the landmarks file with the itkLandmarksReader.
+  //
+  using LandmarksReaderType = itk::LandmarksReader<Dimension>;
+
+  LandmarksReaderType::Pointer landmarksReade
