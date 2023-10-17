27bd8206be90171218c29b1f52e61772676789c9
voyageartist <voyageartist@proton.me>
2023-10-17 19:22:04 +0000

    Fix build breakage

--- test/itkLesionSegmentationMethodTest2.cxx
+++ test/itkLesionSegmentationMethodTest2.cxx
@@ -11,4 +11,33 @@
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notice for more information.
 
-=====================================================================
+=========================================================================*/
+
+#include "itkLesionSegmentationMethod.h"
+#include "itkImage.h"
+#include "itkSpatialObject.h"
+#include "itkImageMaskSpatialObject.h"
+#include "itkDescoteauxSheetnessFeatureGenerator.h"
+#include "itkFrangiTubularnessFeatureGenerator.h"
+#include "itkGradientMagnitudeSigmoidFeatureGenerator.h"
+#include "itkSatoLocalStructureFeatureGenerator.h"
+#include "itkSatoVesselnessFeatureGenerator.h"
+#include "itkTestingMacros.h"
+
+
+int
+itkLesionSegmentationMethodTest2(int itkNotUsed(argc), char * itkNotUsed(argv)[])
+{
+  constexpr unsigned int Dimension = 3;
+
+  using MethodType = itk::LesionSegmentationMethod<Dimension>;
+
+  MethodType::Pointer segmentationMethod = MethodType::New();
+
+  ITK_EXERCISE_BASIC_OBJECT_METHODS(segmentationMethod, LesionSegmentationMethod, ProcessObject);
+
+  using ImageMaskSpatialObjectType = itk::ImageMaskSpatialObject<Dimension>;
+
+  ImageMaskSpatialObjectType::Pointer regionOfInterest = ImageMaskSpatialObjectType::New();
+
+ 
