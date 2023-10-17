59fe8708fbb1064e583ee825a73c6b656e795839
voyageartist <voyageartist@proton.me>
2023-10-17 14:18:14 +0000

    add issue templates

--- test/itkLesionSegmentationMethodTest1.cxx
+++ test/itkLesionSegmentationMethodTest1.cxx
@@ -11,4 +11,33 @@
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notice for more information.
 
-====================================================================
+=========================================================================*/
+
+#include "itkLesionSegmentationMethod.h"
+#include "itkFeatureGenerator.h"
+#include "itkImage.h"
+#include "itkSpatialObject.h"
+#include "itkImageMaskSpatialObject.h"
+#include "itkTestingMacros.h"
+
+
+int
+itkLesionSegmentationMethodTest1(int itkNotUsed(argc), char * itkNotUsed(argv)[])
+{
+  constexpr unsigned int Dimension = 3;
+
+  using MethodType = itk::LesionSegmentationMethod<Dimension>;
+
+  MethodType::Pointer segmentationMethod = MethodType::New();
+
+  ITK_EXERCISE_BASIC_OBJECT_METHODS(segmentationMethod, LesionSegmentationMethod, ProcessObject);
+
+  using ImageMaskSpatialObjectType = itk::ImageMaskSpatialObject<Dimension>;
+
+  ImageMaskSpatialObjectType::Pointer regionOfInterest = ImageMaskSpatialObjectType::New();
+
+  segmentationMethod->SetRegionOfInterest(regionOfInterest);
+
+  const MethodType::SpatialObjectType * regionOfInterestReturned = segmentationMethod->GetRegionOfInterest();
+
+  if (regionOfInterestReturned != regionOfInterest.
