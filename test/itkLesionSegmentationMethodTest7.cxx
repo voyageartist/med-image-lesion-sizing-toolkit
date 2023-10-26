7125a3146065411894833ffd97a1b1a49449a28b
voyageartist <voyageartist@proton.me>
2023-10-26 13:06:31 +0000

    Initialized repository

--- test/itkLesionSegmentationMethodTest7.cxx
+++ test/itkLesionSegmentationMethodTest7.cxx
@@ -8,4 +8,29 @@
   See Copyright.txt or https://www.kitware.com/Copyright.htm for details.
 
      This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or F
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+// The test runs a shape detection level set from user supplied seed points
+// and then runs the shape detection level set with the results from the
+// fast marching to get the final segmentation.
+
+#include "itkLesionSegmentationMethod.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkLandmarksReader.h"
+#include "itkImageMaskSpatialObject.h"
+#include "itkLungWallFeatureGenerator.h"
+#include "itkSatoVesselnessSigmoidFeatureGenerator.h"
+#include "itkSigmoidFeatureGenerator.h"
+#include "itkFastMarchingAndGeodesicActiveContourLevelSetSegmentationModule.h"
+#include "itkMinimumFeatureAggregator.h"
+#include "itkTestingMacros.h"
+
+
+// Applies fast marhching followed by segmentation using geodesic active contours.
+int
+itk
