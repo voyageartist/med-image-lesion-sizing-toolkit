ddede1afce8b2df9f10d6b24913796249d7a4bab
voyageartist <voyageartist@proton.me>
2023-10-29 18:48:00 +0000

    Update HISTORY for the release

--- test/itkLesionSegmentationMethodTest8.cxx
+++ test/itkLesionSegmentationMethodTest8.cxx
@@ -1,7 +1,8 @@
+
 /*=========================================================================
 
   Program:   Lesion Sizing Toolkit
-  Module:    itkLesionSegmentationMethodTest7.cxx
+  Module:    itkLesionSegmentationMethodTest8.cxx
 
   Copyright (c) Kitware Inc.
   All rights reserved.
@@ -25,6 +26,7 @@
 #include "itkImageMaskSpatialObject.h"
 #include "itkLungWallFeatureGenerator.h"
 #include "itkSatoVesselnessSigmoidFeatureGenerator.h"
+#include "itkCannyEdgesFeatureGenerator.h"
 #include "itkSigmoidFeatureGenerator.h"
 #include "itkFastMarchingAndGeodesicActiveContourLevelSetSegmentationModule.h"
 #include "itkMinimumFeatureAggregator.h"
@@ -33,14 +35,17 @@
 
 // Applies fast marhching followed by segmentation using geodesic active contours.
 int
-itkLesionSegmentationMethodTest7(int argc, char * argv[])
+itkLesionSegmentationMethodTest8(int argc, char * argv[])
 {
   if (argc < 3)
   {
     std::cerr << "Missing parameters." << std::endl;
     std::cerr << "Usage: " << argv[0];
-    std::cerr << " landmarksFile inputImage outputImage";
-    std::cerr << " [RMSErrorForGeodesicActiveContour]"
+    std::cerr << " landmarksFile tinputImage toutputImage";
+    std::cerr << " [CannySigma]"
+              << " [CannyUpperThreshold]"
+              << " [CannyLowerThreshold]"
+              << " [RMSErrorForGeodesicActiveContour]"
               << " [IterationsForGeodesicActiveContour]"
               << " [CurvatureScalingForGeodesicActiveContour]"
               << " [PropagationScalingForGeodesicActiveContour]"
@@ -82,11 +87,17 @@ itkLesionSegmentationMethodTest7(int argc, char * argv[])
   using SigmoidFeatureGeneratorType = itk::SigmoidFeatureGenerator<Dimension>;
   SigmoidFeatureGeneratorType::Pointer sigmoidGenerator = SigmoidFeatureGeneratorType::New();
 
+  using CannyEdgesFeatureGeneratorType = itk::CannyEdgesFeatureGenerator<Dimension>;
+  CannyEdgesFeatureGeneratorType::Pointer cannyEdgesGenerator = CannyEdgesFeatureGeneratorType::New();
+
   using FeatureAggregatorType = itk::MinimumFeatureAggregator<Dimension>;
   FeatureAggregatorType::Pointer featureAggregator = FeatureAggregatorType::New();
+
   featureAggregator->AddFeatureGenerator(lungWallGenerator);
   featureAggregator->AddFeatureGenerator(vesselnessGenerator);
+  featureAggregator->AddFeatureGenerator(cannyEdgesGenerator);
   featureAggregator->AddFeatureGenerator(sigmoidGenerator);
+
   lesionSegmentationMethod->AddFeatureGenerator(featureAggregator);
 
   using SpatialObjectType = MethodType::SpatialObjectType;
@@ -102,12 +113,45 @@ itkLesionSegmentationMethodTest7(int argc, char * argv[])
   lungWallGenerator->SetInput(inputObject);
   vesselnessGenerator->SetInput(inputObject);
   sigmoidGenerator->SetInput(inputObject);
+  cannyEdgesGenerator->SetInput(inputObject);
+
   lungWallGenerator->SetLungThreshold(-400);
+
   vesselnessGenerator->SetSigma(1.0);
-  vesselnessGenerator->SetAlpha1(0.5);
+  vesselnessGenerator->SetAlpha1(0.1);
   vesselnessGenerator->SetAlpha2(2.0);
-  sigmoidGenerator->SetAlpha(1.0);
-  sigmoidGenerator->SetBeta(-200.0);
+  vesselnessGenerator->SetSigmoidAlpha(-10.0);
+  vesselnessGenerator->SetSigmoidBeta(80.0);
+
+  sigmoidGenerator->SetAlpha(100.0);
+  sigmoidGenerator->SetBeta(-500.0);
+
+  double spacing[3] = { inputImage->GetSpacing()[0], inputImage->GetSpacing()[1], inputImage->GetSpacing()[2] };
+  double maxSpacing = (spacing[0] > spacing[1] ? spacing[0] : spacing[1]);
+  maxSpacing = (maxSpacing > spacing[2] ? maxSpacing : spacing[2]);
+  if (argc > 4)
+  {
+    maxSpacing = std::stod(argv[4]);
+  }
+  cannyEdgesGenerator->SetSigma(maxSpacing);
+  ITK_TEST_SET_GET_VALUE(maxSpacing, cannyEdgesGenerator->GetSigma());
+
+  double upperThreshold = 150.0;
+  if (argc > 5)
+  {
+    upperThreshold = std::stod(argv[5]);
+  }
+  cannyEdgesGenerator->SetUpperThreshold(upperThreshold);
+  ITK_TEST_SET_GET_VALUE(upperThreshold, cannyEdgesGenerator->GetUpperThreshold());
+
+  double lowerThreshold = 75.0;
+  if (argc > 6)
+  {
+    lowerThreshold = std::stod(argv[6]);
+  }
+  cannyEdgesGenerator->SetLowerThreshold(lowerThreshold);
+  ITK_TEST_SET_GET_VALUE(lowerThreshold, cannyEdgesGenerator->GetLowerThreshold());
+
 
   using SegmentationModuleType = itk::FastMarchingAndGeodesicActiveContourLevelSetSegmentationModule<Dimension>;
   SegmentationModuleType::Pointer segmentationModule = SegmentationModuleType::New();
@@ -116,59 +160,58 @@ itkLesionSegmentationMethodTest7(int argc, char * argv[])
                                 FastMarchingAndGeodesicActiveContourLevelSetSegmentationModule,
                                 SinglePhaseLevelSetSegmentationModule);
 
-
   double maximumRMSError = 0.0002;
-  if (argc > 4)
+  if (argc > 7)
   {
-    maximumRMSError = std::stod(argv[4]);
+    maximumRMSError = std::stod(argv[7]);
   }
   segmentationModule->SetMaximumRMSError(maximumRMSError);
   ITK_TEST_SET_GET_VALUE(maximumRMSError, segmentationModule->GetMaximumRMSError());
 
   unsigned int maximumNumberOfIterations = 300;
-  if (argc > 5)
+  if (argc > 8)
   {
-    maximumNumberOfIterations = std::stoi(argv[5]);
+    maximumNumberOfIterations = std::stoi(argv[8]);
   }
   segmentationModule->SetMaximumNumberOfIterations(maximumNumberOfIterations);
   ITK_TEST_SET_GET_VALUE(maximumNumberOfIterations, segmentationModule->GetMaximumNumberOfIterations());
 
   double curvatureScaling = 1.0;
-  if (argc > 6)
+  if (argc > 9)
   {
-    curvatureScaling = std::stod(argv[6]);
+    curvatureScaling = std::stod(argv[9]);
   }
   segmentationModule->SetCurvatureScaling(curvatureScaling);
   ITK_TEST_SET_GET_VALUE(curvatureScaling, segmentationModule->GetCurvatureScaling());
 
   double propagationScaling = 500.0;
-  if (argc > 7)
+  if (argc > 10)
   {
-    propagationScaling = std::stod(argv[7]);
+    propagationScaling = std::stod(argv[10]);
   }
   segmentationModule->SetPropagationScaling(propagationScaling);
   ITK_TEST_SET_GET_VALUE(propagationScaling, segmentationModule->GetPropagationScaling());
 
   double advectionScaling = 0.0;
-  if (argc > 8)
+  if (argc > 11)
   {
-    advectionScaling = std::stod(argv[8]);
+    advectionScaling = std::stod(argv[11]);
   }
   segmentationModule->SetAdvectionScaling(advectionScaling);
   ITK_TEST_SET_GET_VALUE(advectionScaling, segmentationModule->GetAdvectionScaling());
 
   double stoppingValue = 5.0;
-  if (argc > 9)
+  if (argc > 12)
   {
-    stoppingValue = std::stod(argv[9]);
+    stoppingValue = std::stod(argv[12]);
   }
   segmentationModule->SetStoppingValue(stoppingValue);
   ITK_TEST_SET_GET_VALUE(stoppingValue, segmentationModule->GetStoppingValue());
 
-  double distanceFromSeeds = 2.0;
-  if (argc > 10)
+  double distanceFromSeeds = 0.5;
+  if (argc > 13)
   {
-    distanceFromSeeds = std::stod(argv[10]);
+    distanceFromSeeds = std::stod(argv[13]);
   }
   segmentationModule->SetDistanceFromSeeds(distanceFromSeeds);
   ITK_TEST_SET_GET_VALUE(distanceFromSeeds, segmentationModule->GetDistanceFromSeeds());
@@ -186,6 +229,7 @@ itkLesionSegmentationMethodTest7(int argc, char * argv[])
 
 
   lesionSegmentationMethod->SetInitialSegmentation(landmarksReader->GetOutput());
+
   ITK_TRY_EXPECT_NO_EXCEPTION(lesionSegmentationMethod->Update());
 
 
@@ -197,10 +241,12 @@ itkLesionSegmentationMethodTest7(int argc, char * argv[])
 
   OutputSpatialObjectType::ConstPointer outputObject =
     dynamic_cast<const OutputSpatialObjectType *>(segmentation.GetPointer());
+
   OutputImageType::ConstPointer outputImage = outputObject->GetImage();
 
   using OutputWriterType = itk::ImageFileWriter<OutputImageType>;
   OutputWriterType::Pointer writer = OutputWriterType::New();
+
   writer->SetFileName(argv[3]);
   writer->SetInput(outputImage);
   writer->UseCompressionOn();
@@ -214,6 +260,7 @@ itkLesionSegmentationMethodTest7(int argc, char * argv[])
   lesionSegmentationMethod->AddFeatureGenerator(lungWallGenerator);
 
   ITK_TRY_EXPECT_EXCEPTION(lesionSegmentationMethod->Update());
+
 
   std::cout << "Test finished." << std::endl;
   return EXIT_SUCCESS;
