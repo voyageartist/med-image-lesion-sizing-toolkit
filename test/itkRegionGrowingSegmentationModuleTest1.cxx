98a6115ad1f9860e4a32eff79ff275c12e6c2b8f
voyageartist <voyageartist@proton.me>
2024-01-06 18:27:13 +0000

    Update cabal file

--- test/itkRegionGrowingSegmentationModuleTest1.cxx
+++ test/itkRegionGrowingSegmentationModuleTest1.cxx
@@ -17,4 +17,36 @@
 #include "itkImage.h"
 #include "itkSpatialObject.h"
 #include "itkImageSpatialObject.h"
-#include "itkImageMaskSpatialObject
+#include "itkImageMaskSpatialObject.h"
+
+int
+itkRegionGrowingSegmentationModuleTest1(int itkNotUsed(argc), char * itkNotUsed(argv)[])
+{
+  constexpr unsigned int Dimension = 3;
+
+  using SegmentationModuleType = itk::RegionGrowingSegmentationModule<Dimension>;
+  using SpatialObjectType = SegmentationModuleType::SpatialObjectType;
+
+  SegmentationModuleType::Pointer segmentationModule = SegmentationModuleType::New();
+
+  using ImageSpatialObjectType = itk::ImageSpatialObject<Dimension>;
+
+  ImageSpatialObjectType::Pointer inputObject = ImageSpatialObjectType::New();
+
+  segmentationModule->SetInput(inputObject);
+
+  ImageSpatialObjectType::Pointer featureObject = ImageSpatialObjectType::New();
+
+  segmentationModule->SetFeature(featureObject);
+
+
+  segmentationModule->Update();
+
+  SpatialObjectType::ConstPointer segmentation = segmentationModule->GetOutput();
+
+  segmentationModule->Print(std::cout);
+
+  std::cout << "Class name = " << segmentationModule->GetNameOfClass() << std::endl;
+
+  return EXIT_SUCCESS;
+}
