abd2637d961ffb92168cd93a972d61a742318273
voyageartist <voyageartist@proton.me>
2024-01-29 22:43:45 +0000

    update README [ci skip]

--- test/itkSatoVesselnessFeatureGeneratorTest1.cxx
+++ test/itkSatoVesselnessFeatureGeneratorTest1.cxx
@@ -13,4 +13,36 @@
 
 =========================================================================*/
 
-#include "itkSatoVesselne
+#include "itkSatoVesselnessFeatureGenerator.h"
+#include "itkImage.h"
+#include "itkSpatialObject.h"
+#include "itkImageSpatialObject.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkTestingMacros.h"
+
+
+int
+itkSatoVesselnessFeatureGeneratorTest1(int argc, char * argv[])
+{
+  if (argc < 3)
+  {
+    std::cerr << "Missing parameters." << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " inputImage outputImage [sigma alpha1 alpha2]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  constexpr unsigned int Dimension = 3;
+
+  using InputPixelType = signed short;
+  using OutputPixelType = float;
+
+  using InputImageType = itk::Image<InputPixelType, Dimension>;
+  using OutputImageType = itk::Image<OutputPixelType, Dimension>;
+
+  using ReaderType = itk::ImageFileReader<InputImageType>;
+  using WriterType = itk::ImageFileWriter<OutputImageType>;
+
+  using InputImageSpatialObjectType = itk::ImageSpatialObject<Dimension, InputPixelType>;
+  using OutputImageSpatialObjectT
