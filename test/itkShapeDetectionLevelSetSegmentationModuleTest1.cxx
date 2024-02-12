5d962816728ee48030ae2526e3a9539867ae1a01
voyageartist <voyageartist@proton.me>
2024-02-12 23:05:18 +0000

    // WIP

--- test/itkShapeDetectionLevelSetSegmentationModuleTest1.cxx
+++ test/itkShapeDetectionLevelSetSegmentationModuleTest1.cxx
@@ -38,3 +38,103 @@ itkShapeDetectionLevelSetSegmentationModuleTest1(int argc, char * argv[])
 
 
   constexpr unsigned int Dimension = 3;
+
+  using SegmentationModuleType = itk::ShapeDetectionLevelSetSegmentationModule<Dimension>;
+
+  SegmentationModuleType::Pointer segmentationModule = SegmentationModuleType::New();
+
+  ITK_EXERCISE_BASIC_OBJECT_METHODS(
+    segmentationModule, ShapeDetectionLevelSetSegmentationModule, SinglePhaseLevelSetSegmentationModule);
+
+  using InputImageType = SegmentationModuleType::InputImageType;
+  using FeatureImageType = SegmentationModuleType::FeatureImageType;
+  using OutputImageType = SegmentationModuleType::OutputImageType;
+
+  using InputReaderType = itk::ImageFileReader<InputImageType>;
+  using InputWriterType = itk::ImageFileWriter<InputImageType>;
+  using FeatureReaderType = itk::ImageFileReader<FeatureImageType>;
+  using WriterType = itk::ImageFileWriter<OutputImageType>;
+
+  using RescaleFilterType = itk::RescaleIntensityImageFilter<InputImageType, InputImageType>;
+
+  InputReaderType::Pointer   inputReader = InputReaderType::New();
+  InputWriterType::Pointer   inputWriter = InputWriterType::New();
+  FeatureReaderType::Pointer featureReader = FeatureReaderType::New();
+
+  inputReader->SetFileName(argv[1]);
+
+  ITK_TRY_EXPECT_NO_EXCEPTION(inputReader->Update());
+
+
+  RescaleFilterType::Pointer rescaler = RescaleFilterType::New();
+  rescaler->SetInput(inputReader->GetOutput());
+
+  rescaler->SetOutputMinimum(-4.0);
+  rescaler->SetOutputMaximum(4.0);
+
+  ITK_TRY_EXPECT_NO_EXCEPTION(rescaler->Update());
+
+
+  featureReader->SetFileName(argv[2]);
+
+  ITK_TRY_EXPECT_NO_EXCEPTION(featureReader->Update());
+
+  using InputSpatialObjectType = SegmentationModuleType::InputSpatialObjectType;
+  using FeatureSpatialObjectType = SegmentationModuleType::FeatureSpatialObjectType;
+  using OutputSpatialObjectType = SegmentationModuleType::OutputSpatialObjectType;
+
+  InputSpatialObjectType::Pointer   inputObject = InputSpatialObjectType::New();
+  FeatureSpatialObjectType::Pointer featureObject = FeatureSpatialObjectType::New();
+
+  inputObject->SetImage(rescaler->GetOutput());
+  featureObject->SetImage(featureReader->GetOutput());
+
+  segmentationModule->SetInput(inputObject);
+  segmentationModule->SetFeature(featureObject);
+
+
+  double propagationScaling = 1.0;
+  if (argc > 4)
+  {
+    propagationScaling = std::stod(argv[4]);
+  }
+  segmentationModule->SetPropagationScaling(propagationScaling);
+  ITK_TEST_SET_GET_VALUE(propagationScaling, segmentationModule->GetPropagationScaling());
+
+  double curvatureScaling = 1.0;
+  if (argc > 5)
+  {
+    curvatureScaling = std::stod(argv[5]);
+  }
+  segmentationModule->SetCurvatureScaling(curvatureScaling);
+  ITK_TEST_SET_GET_VALUE(curvatureScaling, segmentationModule->GetCurvatureScaling());
+
+  unsigned int maximumNumberOfIterations = 50;
+  if (argc > 6)
+  {
+    maximumNumberOfIterations = std::stoi(argv[6]);
+  }
+  segmentationModule->SetMaximumNumberOfIterations(maximumNumberOfIterations);
+  ITK_TEST_SET_GET_VALUE(maximumNumberOfIterations, segmentationModule->GetMaximumNumberOfIterations());
+
+
+  ITK_TRY_EXPECT_NO_EXCEPTION(segmentationModule->Update());
+
+  using SpatialObjectType = SegmentationModuleType::SpatialObjectType;
+  SpatialObjectType::ConstPointer segmentation = segmentationModule->GetOutput();
+
+  OutputSpatialObjectType::ConstPointer outputObject =
+    dynamic_cast<const OutputSpatialObjectType *>(segmentation.GetPointer());
+
+  OutputImageType::ConstPointer outputImage = outputObject->GetImage();
+
+  WriterType::Pointer writer = WriterType::New();
+
+  writer->SetFileName(argv[3]);
+  writer->SetInput(outputImage);
+
+  ITK_TRY_EXPECT_NO_EXCEPTION(writer->Update());
+
+  std::cout << "Test finished." << std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ test/itkSigmoidFeatureGeneratorTest1.cxx
@@ -0,0 +1,110 @@
+
+/*=========================================================================
+
+  Program:   Lesion Sizing Toolkit
+  Module:    itkSigmoidFeatureGeneratorTest1.cxx
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See Copyright.txt or https://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "itkSigmoidFeatureGenerator.h"
+#include "itkImage.h"
+#include "itkSpatialObject.h"
+#include "itkImageSpatialObject.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkTestingMacros.h"
+
+
+int
+itkSigmoidFeatureGeneratorTest1(int argc, char * argv[])
+{
+  if (argc < 3)
+  {
+    std::cerr << "Missing parameters." << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " inputImage outputImage [alpha beta]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  constexpr unsigned int Dimension = 3;
+
+  using InputPixelType = signed short;
+  using OutputPixelType = float;
+
+  using InputImageType = itk::Image<InputPixelType, Dimension>;
+  using OutputImageType = itk::Image<OutputPixelType, Dimension>;
+
+  using ReaderType = itk::ImageFileReader<InputImageType>;
+  using WriterType = itk::ImageFileWriter<OutputImageType>;
+
+  using InputImageSpatialObjectType = itk::ImageSpatialObject<Dimension, InputPixelType>;
+  using OutputImageSpatialObjectType = itk::ImageSpatialObject<Dimension, OutputPixelType>;
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  reader->SetFileName(argv[1]);
+
+  ITK_TRY_EXPECT_NO_EXCEPTION(reader->Update());
+
+  using SigmoidFeatureGeneratorType = itk::SigmoidFeatureGenerator<Dimension>;
+  using SpatialObjectType = SigmoidFeatureGeneratorType::SpatialObjectType;
+
+  SigmoidFeatureGeneratorType::Pointer featureGenerator = SigmoidFeatureGeneratorType::New();
+
+  ITK_EXERCISE_BASIC_OBJECT_METHODS(featureGenerator, SigmoidFeatureGenerator, FeatureGenerator);
+
+  InputImageSpatialObjectType::Pointer inputObject = InputImageSpatialObjectType::New();
+
+  InputImageType::Pointer inputImage = reader->GetOutput();
+
+  inputImage->DisconnectPipeline();
+
+  inputObject->SetImage(inputImage);
+
+  featureGenerator->SetInput(inputObject);
+
+  double alpha = 1.0;
+  if (argc > 3)
+  {
+    alpha = std::stod(argv[3]);
+  }
+  featureGenerator->SetAlpha(alpha);
+  ITK_TEST_SET_GET_VALUE(alpha, featureGenerator->GetAlpha());
+
+  double beta = 100.0;
+  if (argc > 4)
+  {
+    beta = std::stod(argv[4]);
+  }
+  featureGenerator->SetBeta(beta);
+  ITK_TEST_SET_GET_VALUE(beta, featureGenerator->GetBeta());
+
+
+  ITK_TRY_EXPECT_NO_EXCEPTION(featureGenerator->Update());
+
+  SpatialObjectType::ConstPointer feature = featureGenerator->GetFeature();
+
+  OutputImageSpatialObjectType::ConstPointer outputObject =
+    dynamic_cast<const OutputImageSpatialObjectType *>(feature.GetPointer());
+
+  OutputImageType::ConstPointer outputImage = outputObject->GetImage();
+
+  WriterType::Pointer writer = WriterType::New();
+
+  writer->SetFileName(argv[2]);
+  writer->SetInput(outputImage);
+  writer->UseCompressionOn();
+
+  ITK_TRY_EXPECT_NO_EXCEPTION(writer->Update());
+
+  std::cout << "Test finished." << std::endl;
+  return EXIT_SUCCESS;
+}
